b2dc3a1d1bd3dc2b9b741fbcd01e66a3aa5c75d1
Colin Batchelor <colin@tantallon.org.uk>
2018-09-21 22:19:50 +0100

    and added default, ntlk-based tagger

--- gaelic_pos/README.md
+++ gaelic_pos/README.md
@@ -2,7 +2,9 @@
 
 This is my local and modified version of Will Lamb (w.lamb@ed.ac.uk)'s Scottish Gaelic part-of-speech tagger and tokeniser.
 
+Needs python3 to run the tests.
+
 See https://bitbucket.org/gaelic_nlp/gaelic_pos/ for the original version.
 
 Colin Batchelor
-2017-09-29.
+2018-09-21
--- gaelic_pos/test_postagger.py
+++ gaelic_pos/test_postagger.py
@@ -9,7 +9,10 @@ class Test(unittest.TestCase):
     def tearDown(self):
         self.tagger = None
 
-    def test_basic(self):
+    def test_default(self):
+        self.assertEqual(self.tagger.tagfile_default(["Is","mise","Cailean"]), [("Is","Wp-i"),("mise","Pp1s--e"),("Cailean","Nn-mn")])
+        
+    def test_retag(self):
         self.assertEqual(self.tagger._retag(["tha", "mi", "a'", "dol", "dhachaigh"], ["V-p", "Pp1s", "Sp", "Nv", "Rs"]), [("tha","V-p"),("mi","Pp1s"),("a'","Sp"),("dol","Nv"),("dhachaigh","Rs")])
         
 if __name__ == '__main__':
