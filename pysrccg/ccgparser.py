0c01836079429ffab243904b77a0975edfe84349
colinbatchelor <colin.r.batchelor@gmail.com>
2019-04-22 21:02:03 +0100

    tidying up by removing shift--reduce parser

--- pysrccg/ccgparser.py
+++
@@ -1,8 +0,0 @@
-def decode(input, agenda, list, N, grammar):
-  agenda.append(input[0])
-  candidate_output = []
-  while (len(agenda)> 0):
-    list = []
-  return candidate_output
-
-print(decode([('Tha','Vs')],[],[],2,[]))
