892c1a8629651d8d1ec341b4063545b78015a157
Colin Batchelor <colin@tantallon.org.uk>
2019-07-08 07:03:36 +0100

    renamed main file; adjustments to cross-validation pipeline

--- strip_comments.py
+++ strip_comments.py
@@ -1,6 +1,7 @@
+import re
 import sys
 with open(sys.argv[1]) as f, open(sys.argv[2], 'w') as g:
     for line in f:
-        if not line.startswith('#'):
+        if not line.startswith('#') and not re.match("^\d+-",line):
             g.write(line)
         
