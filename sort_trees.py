70f7522fcb585c214a531d0159d412c76d3240a5
Colin Batchelor <colin.r.batchelor@gmail.com>
2020-09-06 11:19:32 +0100

    merged conversations and sport into arcosg_unseen

--- sort_trees.py
+++ sort_trees.py
@@ -4,6 +4,8 @@ import pyconll
 corpus = pyconll.load_from_file(sys.argv[1])
 trees = []
 with open(sys.argv[2],'w') as clean:
+    for sentence in corpus:
+        print(sentence.id)
     for sentence in sorted(corpus, key= lambda x: x.id):
         clean.write(sentence.conll())
         clean.write('\n\n')
