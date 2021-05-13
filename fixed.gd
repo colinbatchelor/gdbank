2b452a481dc1fbb3180900a97a694a07dae1ba56
Colin Batchelor <colin.r.batchelor@gmail.com>
2021-05-13 21:00:00 +0100

    forgot to commit fixed.gd

---
+++ fixed.gd
@@ -0,0 +1,219 @@
+'n déidh
+'n diugh
+'n ear
+'n iar
+'n seo
+'n uair
+'n uairsin
+'s an e
+'s e
+'s i
+a bhòn-uiridh
+a chaoidh
+a cheana
+a cheile
+a chèile
+a chéile
+a chionn
+a deas
+a dh'aindeoin
+a dh'ainneoin
+a dh'aithghearr
+a dh'aona-ghnothaich
+a h-uile
+a lùib
+a mach
+a màireach
+a muigh
+a neisd
+a nis
+a nise
+a nochd
+a nuas
+a raoir
+a réir
+a rèir
+a riamh
+a rithist
+a seo
+a seothach
+a shin
+a shineach
+a sineach
+a staigh
+a steach
+a stigh
+a thaobh
+a thoireadh
+a thoradh
+a tuath
+a' diugh
+air beulaibh
+air bheulaibh
+air bith
+air choireigin
+air choreigin
+air cùlaibh
+air dheireadh
+air falbh
+air feadh
+air leth
+air neo
+air sgath
+air sgàth
+air sgàth 's
+air son
+air thùs
+airson 's
+am bliadhna
+am broinn
+am feadh
+am feadh is
+am measg
+am muigh
+an an
+an àird
+an àirde
+an ath bhliadhna
+an ath-bhliadhna
+an ath-oidhch'
+an ath-oidhche
+an ceart-uair
+an ceartuair
+an cois
+an còmhnaidh
+an deas
+an dè
+an deidh
+an dèidh
+an déidh
+an diugh
+an dràsda
+an dràsta
+an e
+an ear
+an earar
+an earras
+an iar
+an iarar
+an iaras
+an ìre mhath
+an làrna-mhàireach
+an lùib
+an measg
+an raoir
+an sean
+an seothach
+an sin
+an sineach
+an siod
+an siud
+an siudach
+an tac
+an taca ri
+an taca ris
+an uairsin
+an uiridh
+an uirigh
+an toiseach
+an uair
+ann a sheo
+ann a sheothach
+ann a shin
+ann a shiodach
+ann a shiud
+ann a shiudach
+ann a-seo
+ann a-seothach
+ann a-sin
+ann a-sineach
+ann a-siud
+ann a'
+ann a:
+ann am
+ann an
+ann an seo
+anns a'
+anns a' bhad
+anns an
+anns na
+aon uair 's
+as aonais
+ás aonais
+às aonais
+as bith
+as deidh
+as dèidh
+as déidh
+b' an e
+b' e
+b' i
+bho chionn
+bho chionn ghoirid
+bho thoiseach
+car son
+chan an e
+chan e
+chionn 's
+cia mheud
+cò mheud
+có dhiubh
+dè cho
+dè mar
+dé mar
+fa leth
+fad 's
+fad as
+fhad 's
+fhad agus
+fiù 's
+fo dheireadh
+gar bith có
+ge b'e dé
+ge brith
+ge brith dé
+gu bè gu dè
+gu dé
+gu ìre mhath
+gu ruige
+gun e
+gur an e
+gur e
+gur h-e
+is an e
+is e
+is i
+ma dheidhinn
+ma dheireadh
+ma sgaoil
+ma tha
+ma thà
+mar an ceudna
+mar bu trice
+mo bheulaibh
+mu 'n
+mu choinneamh
+mu choinneimh
+mu chuairt
+mu dheas
+mu dheidhinn
+mu dheireadh
+mu dheireadh thall
+mu thràth
+mu thuath
+nach an e
+na b'
+na bu
+nach e
+o chionn
+o'n uairsin
+os cionn
+ri cheile
+ri chèile
+ri chéile
+ri linn
+ri linn 's
+sa bhad
+sam bidh
+sam bith
+uair sin
